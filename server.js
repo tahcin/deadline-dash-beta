9f711d824af25d5d59239345a1643dfc06c2aec8
Tahcin Sarwar <88557978+tahcin@users.noreply.github.com>
2025-03-13 23:49:28 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const app = express();
 
 app.use(bodyParser.json());
 
-const VAPID_PUBLIC_KEY = 'YOUR_VAPID_PUBLIC_KEY';
-const VAPID_PRIVATE_KEY = 'YOUR_VAPID_PRIVATE_KEY';
+const VAPID_PUBLIC_KEY = 'BEY0n3-H3bPxVfrCZ6x1MR9S0B2jmelnCLCipewOKB_GAPb_2hJcC8R7h0pcQs4t9pBdW9PWFiWwFYURbGkT8is';
+const VAPID_PRIVATE_KEY = 'EawIsn7lwsmpvrWoMMgq80YztBjOlF5TiXF-Osx1crw';
 
 webpush.setVapidDetails(
-    'mailto:your-email@example.com',
+    'mailto:tahcin49@gmail.com',
     VAPID_PUBLIC_KEY,
     VAPID_PRIVATE_KEY
 );
