afbef4a0d9be1e7c87e3f996e9683dfce956418f
Tahcin Sarwar <88557978+tahcin@users.noreply.github.com>
2025-03-17 04:58:06 +0530

    Update script.js

--- public/script.js
+++ public/script.js
@@ -93,4 +93,11 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
+//PWA
+
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("/service-worker.js")
+    .then(reg => console.log("Service Worker Registered!", reg))
+    .catch(err => console.error("Service Worker registration failed", err));
+}
 
